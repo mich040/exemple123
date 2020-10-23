48a99c9a4776af5c3b2db5d8af84c3f8c4190826
mich040 <73345099+mich040@users.noreply.github.com>
2020-10-23 14:50:50 +0100

    Update dd.py

--- dd.py
+++ dd.py
@@ -1,49 +1,38 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[ ]:
+# YouTube Video: https://www.youtube.com/watch?v=mP_Ln-Z9-XY
+import smtplib
 
+EMAIL_ADDRESS = 'yabous.xman@gmail.com'
+PASSWORD = 'hrasveswuoqkgorj'
 
-while(1):
-    
-    
-
-    import smtplib
-    import time
-
-    from pytrends.request import TrendReq
-
-    # Only need to run this once, the rest of requests will use the same session.
-    pytrend = TrendReq()
-
-    # Create payload and capture API tokens. Only needed for interest_over_time(), interest_by_region() & related_queries()
-    pytrend.build_payload(kw_list=['tshirt'],timeframe='now 7-d',geo='US')
-
-
-    # Related Queries, returns a dictionary of dataframes
-    related_queries_dict = pytrend.related_queries()
-    D=related_queries_dict['tshirt']['rising']
-
-
-    gmail_user = 'yabous.xman@gmail.com'
-    gmail_password = 'qbnrkjfgestijpwu'
-
+def send_email(subject, msg):
     try:
-            server = smtplib.SMTP('smtp.gmail.com:587')
-            server.ehlo()
-            server.starttls()
-            server.login(gmail_user, gmail_password)
-            msg=D
-            message = 'Subject: {}\n\n{}'.format(subject, msg)
-            server.sendmail(gmail_user, gmail_user, message)
-            server.quit()
-            print(msg)
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(EMAIL_ADDRESS,PASSWORD)
+        message = 'Subject: {}\n\n{}'.format(subject, msg)
+        server.sendmail(EMAIL_ADDRESS,EMAIL_ADDRESS, message)
+        server.quit()
+        print("Success: Email sent!")
     except:
-        print ('Something went wrong...')
-        
-        
-        
+        print("Email failed to send.")
 
 
-    time.sleep( 30)
+subject = "Test subject"
 
+from pytrends.request import TrendReq
+
+# Only need to run this once, the rest of requests will use the same session.
+pytrend = TrendReq()
+
+# Create payload and capture API tokens. Only needed for interest_over_time(), interest_by_region() & related_queries()
+pytrend.build_payload(kw_list=['tshirt'],timeframe='now 7-d',geo='US')
+
+
+# Related Queries, returns a dictionary of dataframes
+related_queries_dict = pytrend.related_queries()
+D=related_queries_dict['tshirt']['rising']
+
+
+send_email(subject, D)
